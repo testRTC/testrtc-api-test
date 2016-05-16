1938b84e92d37b2f40ce3a0a536da963f14bef8b
Muly Oved <mulyoved@gmail.com>
2016-05-16 12:36:10 +0300

    should pass test as is

--- test/sample.js
+++ test/sample.js
@@ -1,22 +1,23 @@
-var assert = require('chai').assert;
+//var assert = require('chai').assert;
 var testRTCApi = require('../src/index');
-
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 describe('call testrtc api', function () {
-  var apikey = 'bc51597a-a11d-4ddf-8fbc-c223a305368b';
-  var apiUrl = 'https://api.testrtc.com/v1';
+  //var apikey = 'bc51597a-a11d-4ddf-8fbc-c223a305368b';
+  var apikey = 'de001d2f-2d89-4bc7-8287-6a62d4852060';
+  var apiUrl = 'https://api.testrtc.com/v1s1';
   var auth = {type: 'apiKey', name: 'apikey', in: 'header', apiKey: apikey};
   var api;
-
   before(function() {
     var apiClient = new testRTCApi.ApiClient();
     apiClient.basePath = apiUrl;
     apiClient.authentications['apikey'] = auth;
     api = new testRTCApi.DefaultApi(apiClient);
   });
-
   it('findTestById', function(done) {
-    api.findTestById('567a31aa7524831000e541d9', function(err, result) {
+    this.timeout(20000);
+    //'567a31aa7524831000e541d9'
+    var testId = '56b079c0a61eb11100898e6a';
+    api.findTestById(testId, function(err, result) {
       console.log(result);
       done(err);
     });
